95b279fb7f963ab8ca916c7e77ab41c56c6854bf
Klemen Košir <klemen.kosir@kream.io>
2015-12-25 23:38:26 +0100

    Implement minimal C library headers

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -2,5 +2,10 @@
 #define _STDLIB_H
 
 __attribute__((__noreturn__)) void abort(void);
+int atexit(void (*)(void));
+int atoi(const char *);
+void free(void *);
+char* getenv(const char *);
+void* malloc(size_t);
 
 #endif
---
+++ libc/include/unistd.h
@@ -0,0 +1,11 @@
+#ifndef _UNISTD_H
+#define _UNISTD_H
+
+#include <sys/types.h>
+
+int execv(const char *, char *const[]);
+int execve(const char *, char *const[], char *const[]);
+int execvp(const char *, char *const[]);
+pid_t fork(void);
+
+#endif
