65025440b036feaf914b7087af915eb559d414d7
Klemen Košir <klemen.kosir@kream.io>
2015-12-28 16:54:43 +0100

    Add int64_t and uint64_t data types

--- libc/include/stdint.h
+++ libc/include/stdint.h
@@ -6,9 +6,19 @@
 typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
+#if __WORDSIZE == 64
+typedef long int64_t;
+#else
+typedef long long int64_t;
+#endif
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
+#if __WORDSIZE == 64
+typedef unsigned long uint64_t;
+#else
+typedef unsigned long long uint64_t;
+#endif
 
 #endif
